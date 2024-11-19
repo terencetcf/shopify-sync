319a4fa14eca7398884e5a155294b2b5c4195875
Terence Tai (terencetcf) <terencetcf@hotmail.com>
2024-11-19 14:51:04 +0900

    Initial Commit

---
+++ ShopifySync/ShopifySyncApp.swift
@@ -0,0 +1,17 @@
+//
+//  ShopifySyncApp.swift
+//  ShopifySync
+//
+//  Created by Terence Tai on 19/11/2024.
+//
+
+import SwiftUI
+
+@main
+struct ShopifySyncApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ShopifySyncTests/ShopifySyncTests.swift
@@ -0,0 +1,17 @@
+//
+//  ShopifySyncTests.swift
+//  ShopifySyncTests
+//
+//  Created by Terence Tai on 19/11/2024.
+//
+
+import Testing
+@testable import ShopifySync
+
+struct ShopifySyncTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
